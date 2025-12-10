1d1a40c4c8e1e0370c039c555eac0e3f4a5d59d3
DecSeung <jungsi1217@gmail.com>
2025-12-10 12:34:54 +0900

    fix: baseUrl 수정

--- src/lib/utils/apiUtils.ts
+++ src/lib/utils/apiUtils.ts
@@ -4,7 +4,7 @@ type FetchOptions = {
   revalidate?: number // Next ISR (ex: 10 = 10초 후 자동 재검증)
 }
 
-const baseUrl = 'http://localhost:4000'
+const baseUrl = process.env.NEXT_PUBLIC_API_URL
 
 async function fetchWithAuth(
   url: string,
