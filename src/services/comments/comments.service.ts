27f7a0762a331b6be045f1f8bacba4ec748d351a
DecSeung <jungsi1217@gmail.com>
2026-01-06 21:12:07 +0900

    fix: comment.service.ts 파일 apiUtils 변경에 따른 수정

--- src/services/comments/comments.service.ts
+++ src/services/comments/comments.service.ts
@@ -1,4 +1,4 @@
-import api from '@/lib/utils/apiUtils'
+import { api } from '@/lib/utils/apiUtils'
 import { UserInfo } from '@/types/auth'
 
 interface PostCommentRequest {
@@ -6,8 +6,6 @@ interface PostCommentRequest {
   comment: string
   shortsId: string
 }
-
-const comment = api()
 
 export const commentApi = {
   // 해당 숏폼 댓글 조회
@@ -18,14 +16,14 @@ export const commentApi = {
     return response.json()
   },
 
-  postComment: async (data: PostCommentRequest) => {
-    const response = await comment.post(
-      `http://localhost:4000/api/v1/shorts/${data.shortsId}/comments`,
-      data,
-      { cache: 'no-store' },
-    )
-    const result = await response.json()
-    if (!response.ok) throw new Error(result.message || '댓글 등록 실패')
-    return result
-  },
+  // postComment: async (data: PostCommentRequest) => {
+  //   const response = await api.post(
+  //     `http://localhost:4000/api/v1/shorts/${data.shortsId}/comments`,
+  //     data,
+  //     { cache: 'no-store' },
+  //   )
+  //   const result = await response.json()
+  //   if (!response.ok) throw new Error(result.message || '댓글 등록 실패')
+  //   return result
+  // },
 }
