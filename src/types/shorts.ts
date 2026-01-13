c726fc81e4b742cec2d9f7b7366a9586b1e877de
DecSeung <jungsi1217@gmail.com>
2026-01-13 18:42:48 +0900

    fix: shorts 타입 파일명 수정

---
+++ src/types/shorts.ts
@@ -0,0 +1,20 @@
+export interface ShortsUploader {
+  userId: number
+  nickname: string
+  profileUrl: string
+}
+
+export interface ShortsCategory {
+  categoryId: number
+  name: string
+}
+
+export interface ShortsDetail {
+  shortsId: string
+  title: string
+  description: string
+  videoUrl: string
+  thumbnailUrl: string
+  uploader: ShortsUploader
+  category: ShortsCategory
+}
